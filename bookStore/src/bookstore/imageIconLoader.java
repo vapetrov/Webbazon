11add0c3c4410c1e91f12303ef20a5e85408fcab
Vassily <Vassily@Vassily-PC.local>
2014-05-11 20:10:44 -0500

    Fixed catch statement

--- bookStore/src/bookstore/imageIconLoader.java
+++ bookStore/src/bookstore/imageIconLoader.java
@@ -8,6 +8,8 @@ package bookstore;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 
 /**
@@ -19,21 +21,33 @@ public class imageIconLoader {
 
     private ImageIcon[] images;
 
-    public imageIconLoader() throws URISyntaxException, MalformedURLException {
-
-        File resources = new File(getClass().getResource("images").toURI());
-        File[] icons = resources.listFiles();
-
-        images = new ImageIcon[icons.length];
-
-        int count = 0;
-        for (File f : icons) {
-            images[count] = new ImageIcon(f.toURI().toURL());
-            count++;
+    public imageIconLoader(){
+        try {
+            File resources = new File(getClass().getResource("images").toURI());
+            File[] icons = resources.listFiles();
+            
+            images = new ImageIcon[icons.length];
+            
+            int count = 0;
+            for (File f : icons) {
+                images[count] = new ImageIcon(f.toURI().toURL());
+                count++;
+            }
+        } catch (URISyntaxException ex) {
+            System.out.println("Images couldn't be loaded.");
+            images = null;
+        } catch (MalformedURLException ex) {
+            System.out.println("Images couldn't be loaded.");
+            images = null;
         }
 
     }
 
+    /**
+     * Obtains an array of images for displaying
+     * @return A list of all available images in the images package
+     * Precondition: Images are available and are in the right order.
+     */
     public ImageIcon[] getImages() {
         return images;
     }
