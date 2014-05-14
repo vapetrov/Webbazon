092ab6f44c52bc5e20b2f05250ea59dc16264ad9
Vassily <Vassily@Vassily-PC.local>
2014-05-13 19:34:43 -0500

    Components look better

--- bookStore/src/bookstore/bookStatus.java
+++ bookStore/src/bookstore/bookStatus.java
@@ -1,11 +1,15 @@
 
 package bookstore;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.TitledBorder;
 
 /**
@@ -38,21 +42,32 @@ public class bookStatus extends JPanel {
         this.setLayout(new GridLayout());
 
         JPanel selectedBook = new JPanel();
+        //selectedBook.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        
         selectedBook.setLayout(new BoxLayout(selectedBook, BoxLayout.Y_AXIS));
-        selectedBook.setBorder(new TitledBorder("Selected Book:"));
+        selectedBook.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(0, 10, 10, 5), new TitledBorder("Selected Book:")));
         selectedBook.add(new JLabel("Author: J.K. Rowling"));
         selectedBook.add(new JLabel("ISBN: 4523453452345"));
         selectedBook.add(new JLabel("Books available: 9"));
         selectedBook.add(new JLabel("cost: $19.99"));
         add(selectedBook);
+        
+        for(Component e: selectedBook.getComponents()){
+            ((JComponent)e).setBorder(BorderFactory.createEmptyBorder(1, 5, 1, 5));
+        }
 
         JPanel totalBook = new JPanel();
+        //totalBook.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
         totalBook.setLayout(new BoxLayout(totalBook, BoxLayout.Y_AXIS));
-        totalBook.setBorder(new TitledBorder("Book total:"));
+        totalBook.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(0, 5, 10, 10), new TitledBorder("Book total:")));
         totalBook.add(new JLabel("Books sold: 12"));
         totalBook.add(new JLabel("Total Revenue: $53.34"));
         add(totalBook);
 
+        for(Component e: totalBook.getComponents()){
+            ((JComponent)e).setBorder(BorderFactory.createEmptyBorder(1, 5, 1, 5));
+        }
+        
         updateSize();
         
 
