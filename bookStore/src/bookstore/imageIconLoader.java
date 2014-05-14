8825f63b74ccf47707ae1f51455c87767d257bf4
Vassily <Vassily@Vassily-PC.local>
2014-05-13 21:00:58 -0500

    Proper icon loading

--- bookStore/src/bookstore/imageIconLoader.java
+++ bookStore/src/bookstore/imageIconLoader.java
@@ -5,9 +5,10 @@
  */
 package bookstore;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -21,30 +22,47 @@ public class imageIconLoader {
 
     private ImageIcon[] images;
 
-    public imageIconLoader(){
-        try {
-            File resources = new File(getClass().getResource("images").toURI());
-            File[] icons = resources.listFiles();
-            
-            images = new ImageIcon[icons.length];
-            
-            int count = 0;
-            for (File f : icons) {
-                images[count] = new ImageIcon(f.toURI().toURL());
-                count++;
+    public imageIconLoader() {
+
+        ArrayList<ImageIcon> lister = new ArrayList<ImageIcon>();
+        int count = 0;
+        boolean notDone = true;
+        while (notDone) {
+            InputStream input = this.getClass().getResourceAsStream("images/" + count + ".png");
+
+            if (input == null) {
+                notDone = false;
+                System.out.println("good");
+            } else {
+                lister.add(new ImageIcon(reader(input)));
             }
-        } catch (URISyntaxException ex) {
-            System.out.println("Images couldn't be loaded.");
-            images = null;
-        } catch (MalformedURLException ex) {
-            System.out.println("Images couldn't be loaded.");
-            images = null;
+
+            count++;
+
         }
 
+        images = new ImageIcon[lister.size()];
+        lister.toArray(images);
+
+    }
+
+    private byte[] reader(InputStream stream) {
+        int bytesRead;
+        byte[] buffer = new byte[1000];
+        ByteArrayOutputStream writer = new ByteArrayOutputStream();
+        try {
+            while ((bytesRead = stream.read(buffer)) != -1) {
+                writer.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException ex) {
+            return null;
+        }
+        return writer.toByteArray();
     }
 
     /**
      * Obtains an array of images for displaying
+     *
      * @return A list of all available images in the images package
      * Precondition: Images are available and are in the right order.
      */
