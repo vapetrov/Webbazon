13290058a7a1406e871a8cd8348820518390df7a
Vassily Petrov <github@vassily.org>
2014-05-31 22:43:17 -0500

    removed tester items

--- bookStore/src/bookstore/BookStore.java
+++ bookStore/src/bookstore/BookStore.java
@@ -21,12 +21,15 @@ public class BookStore {
 
         Inventory mainInventory = new Inventory();
 
+        
+        // test items
+        /*
         mainInventory.addItem(new InventoryItem(new Book(12.99, "234234DF", "J.K. Rowling", "Harry Potter"), 12));
         mainInventory.addItem(new InventoryItem(new Book(19.95, "asdf234", "Scott Fitzgerald", "The Great Gatsby"), 14));
         mainInventory.addItem(new InventoryItem(new Book(99.99, "1234abcd", "Leo Tolstoy", "War and Peace"), 16));
         mainInventory.addItem(new InventoryItem(new Book(3.00, "dd4523", "John Steinbeck", "Of Mice and Men"), 18));
         mainInventory.addItem(new InventoryItem(new Book(34.00, "34564576dgf", "Mr.Webb", "AP Comp. Sci A"), 999));
-
+        */
         mainGUI.make(mainInventory);
 
         
